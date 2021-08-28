8cc52bba5d9a8a468d9b6c7291b6de189360469a
Luis Gonzalez <luis_gonsu@hotmail.com>
2021-08-27 21:15:28 -0500

    prueba de ssh

--- Generador_contrasena_git.py
+++ Generador_contrasena_git.py
@@ -27,7 +27,7 @@ def generar_contrasena():
 
 def run():
     contrasena = generar_contrasena()
-    print("bienvenido la nueva contraseña para ti es la siguiente: " + contrasena) 
+    print("bienvenido la nueva contraseña 2.0 para ti es la siguiente: " + contrasena) 
     print("la segunda contraseña para ti rama 2 es la siguiente: " + contrasena) 
 
 if __name__ == "__main__":
